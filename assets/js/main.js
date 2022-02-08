c2a0d38fdcaa26bf2dfba49711acfa908211fa66
Waldemar Teichreb <96541856+WaldiTea@users.noreply.github.com>
2022-02-08 13:12:04 +0100

    first commit

---
+++ assets/js/main.js
@@ -0,0 +1,30 @@
+/*---lev1_14---*/
+console.log('-----lev1_14-----');
+
+console.log(Boolean(100));
+console.log(Boolean(3.14));
+console.log(Boolean(-15));
+console.log(Boolean("hello"));
+console.log(Boolean("false"));
+console.log(Boolean(1 + 7 + 3.14));
+console.log(Boolean(0));
+console.log(Boolean(-0));
+console.log(Boolean(NaN));
+console.log(Boolean(null));
+console.log(Boolean(false));
+
+/*---lev1_15---*/
+console.log('-----lev1_15-----');
+
+let num = 5;
+let anotherNum = 8;
+
+console.log(num == anotherNum);
+console.log(num > anotherNum);
+console.log(num < anotherNum);
+console.log(num >= anotherNum);
+console.log(num <= anotherNum);
+
+console.log(num == '5');
+console.log(num != 8);
+console.log(num != '5');
